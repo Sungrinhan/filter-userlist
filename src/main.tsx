205cc1a8cf18570df33536184e55fc69911a8c3b
Sungrinhan <sungrin92@gmail.com>
2023-05-01 21:24:24 +0900

    Feat: redux 모듈 설치에 따른 main.tsx 수정

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,19 @@ import { App } from "./App";
 import { initialize } from "../external/initialize";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { store } from "./store";
+import { Provider } from "react-redux";
+
 const queryClient = new QueryClient();
 
 initialize().then(() =>
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-      <QueryClientProvider client={queryClient}>
+      {/* <QueryClientProvider client={queryClient}> */}
+      <Provider store={store}>
         <App />
-      </QueryClientProvider>
+        {/* </QueryClientProvider> */}
+      </Provider>
     </React.StrictMode>
   )
 );
