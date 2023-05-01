33a6fb7c6b5c062bf187c1f9d344ee0f97465811
Sungrinhan <sungrin92@gmail.com>
2023-05-01 14:36:33 +0900

    Feat: tanstack Query 설치 및 설정
    1. yarn add @tanstack/react-query
    2. main.tsx 에 queryclientprovider 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { initialize } from "../external/initialize";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 initialize().then(() =>
   ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </React.StrictMode>
   )
 );
