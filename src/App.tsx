17907aaa126ee764794a9f6e51f405d2985db7e5
Sungrinhan <sungrin92@gmail.com>
2023-05-01 21:23:40 +0900

    Fix: 컴포넌트화에 따른 App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,16 @@
+import { useEffect } from "react";
 import "./App.css";
 
+import ControlsContainer from "./components/SearchBox";
+import UserTable from "./components/UserTable";
+
+import { useGetIdNameQuery, useGetLetterQuery } from "./store/services/user";
+
 export const App = () => {
-  const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
-  const upperCases = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
-
-  const lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
-
-  const idNameList: [string, string][] = [
-    ["E3hI0gAc7D", "Nico Burris"],
-    ["ABCDEFGHIJ", "Warren Bullock"],
-  ];
-
   return (
     <div className="app">
-      <div id="controls">
-        <input id="searchbar" type="text" />
-
-        <div id="selectbox">
-          <div className="selectbox">
-            <ul>
-              {digits.map((digit) => (
-                <li key={digit}>
-                  <input id={digit} type="checkbox" />
-                  <label htmlFor={digit}>{digit}</label>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="selectbox">
-            <ul>
-              {upperCases.map((letter) => (
-                <li key={letter}>
-                  <input id={letter} type="checkbox" />
-                  <label htmlFor={letter}>{letter}</label>
-                </li>
-              ))}
-            </ul>
-          </div>
-
-          <div className="selectbox">
-            <ul>
-              {lowerCases.map((letter) => (
-                <li key={letter}>
-                  <input id={letter} type="checkbox" />
-                  <label htmlFor={letter}>{letter}</label>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </div>
-
-      <ul className="idnamelist">
-        {idNameList.map(([id, name]) => (
-          <li key={id}>{`${id} - ${name}`}</li>
-        ))}
-      </ul>
+      <ControlsContainer />
+      <UserTable />
     </div>
   );
 };
