98cbf7e83155bcb4c0d98f79839c67c1e87d0ae9
Sungrinhan <sungrin92@gmail.com>
2023-05-01 21:17:24 +0900

    Feat: types 폴더 및 파일 추가

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export type IDNameList = IdName[];
+
+export type IdName = [string, string];
+
+export interface LetterList {
+  digits: string[];
+  lowerCases: string[];
+  upperCases: string[];
+}
